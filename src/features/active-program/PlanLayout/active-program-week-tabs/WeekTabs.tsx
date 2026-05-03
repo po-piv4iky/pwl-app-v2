cd56d1f61e207461d84e59d5d173ffcad2880dde
Ivan <www.ko4ca@gmail.com>
2026-05-03 22:58:53 +0300

    add swipper in weekTabs component and pagination slider

--- src/features/active-program/PlanLayout/active-program-week-tabs/WeekTabs.tsx
+++ src/features/active-program/PlanLayout/active-program-week-tabs/WeekTabs.tsx
@@ -2,9 +2,19 @@ import css from "./WeekTabs.module.scss"
 import { Circle, CheckCircle } from "lucide-react"
 import clsx from "clsx"
 import { useActiveProgramStore } from "@/store/active-program.store"
-import { motion } from "framer-motion"
+import { Swiper, SwiperSlide } from "swiper/react"
+import { 
+  FreeMode,      // свободный скролл
+  Pagination,    // точки навигации
+  Navigation,    // кнопки "вперёд/назад"
+  Autoplay,      // автопрокрутка
+  EffectFade     // эффект затухания
+} from 'swiper/modules';// import { Pagination } from 'swiper/modules'; // Импортируем модуль пагинации
+import "swiper/css"
+import "swiper/css/free-mode"
+import "swiper/css/pagination"
 
-// 🚀 Золотое правило:
+// 🚀 Золотое правило Framer-motion:
 // initial → что было до анимации
 // animate → что стало после анимации
 // exit → что будет при удалении
@@ -24,14 +34,35 @@ export default function WeekTabs() {
   return (
     <section className={css.weeksTabsContainer}>
       <h5>Выберите неделю</h5>
-      <div className={css.scrollWrapper}>
-
-          <motion.div 
-            className={css.buttonsTrack}
-            initial={{ opacity: 0, x: 16 }}
-            animate={{ opacity: 1, x: 0 }}
-            transition={{ duration: 0.25 }}
-          >
+      <Swiper
+        // slidesPerView='auto'  //   // автоширина
+        // freeMode // свободный скролл
+        // slidesPerView={1.15} // сколько видно
+        modules={[FreeMode, Pagination, Navigation, Autoplay, EffectFade]}  // подключает функциональность
+        className={css.swiper}
+        spaceBetween={12} //расстояние между слайдами px
+        breakpoints={{ // адаптив
+          360: {
+            slidesPerView: 2,  //если ширина >= 480px → показываем 1.6 карточки
+          },
+          480: {
+            slidesPerView: 3,  //если ширина >= 480px → показываем 1.6 карточки
+          },
+          768: {
+            slidesPerView: 4,
+          },
+          1030: {
+            slidesPerView: 5,
+          },
+          1280: {
+            slidesPerView: 8,
+          },
+        }}
+        pagination={{ 
+        clickable: true,
+        dynamicBullets: true 
+      }}
+      >
             {weeks.map((week) => {
               const weekNumber = week.weekNumber
               const isActive = weekNumber === currentWeek
@@ -40,32 +71,30 @@ export default function WeekTabs() {
               
               const isCompletedWeek = completedWorkouts === totalWorkouts
               return (
-                <motion.button
-                  key={weekNumber}
-                  onClick={() => setCurrentWeek(weekNumber)}
-                  className={clsx(
-                  css.buttonWeek,
-                  isActive && css.active,
-                  )}
-                  initial={{opacity: 0, y: 8}}
-                 animate={{opacity: 1, y: 0}}
-                  transition={{duration: 0.2}}
-                  whileHover={{y: -2}}
-                  whileTap={{ scale: 0.97 }}
-                >
-                  <div className={css.iconAndWeekBlock}>
-                {isCompletedWeek ? <CheckCircle /> : <Circle />}
-                    <span>Неделя {week.weekNumber}</span>
-                  </div>
+                <SwiperSlide key={weekNumber} className={css.slide}>
+                    <button
+                      onClick={() => setCurrentWeek(weekNumber)}
+                      className={clsx(
+                      css.buttonWeek,
+                      isActive && css.active,
+                      )}
+                    >
+                      <div className={css.iconAndWeekBlock}>
+                        {isCompletedWeek ? <CheckCircle /> : <Circle />}
+                        <span>Неделя {week.weekNumber}</span>
+                      </div>
     
-              <span className={css.progress}>
-                    {completedWorkouts}/{totalWorkouts} завершено
-                  </span>
-                </motion.button>
+                      <span className={css.progress}>
+                        {completedWorkouts}/{totalWorkouts} завершено
+                      </span>
+                    </button>
+                </SwiperSlide>
               )
            })}
-          </motion.div>
-      </div>
+      </Swiper>
+      
+         
+      
     </section>
   )
 }
